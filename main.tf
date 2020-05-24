c8149b81652bf2af266e84e0b991086786eab452
Alex Seymour <axeal@users.noreply.github.com>
2020-05-24 23:29:33 +0200

    Run terraform fmt

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
-    backend "remote" {
-        organization = "axeal"
+  backend "remote" {
+    organization = "axeal"
 
-        workspaces {
-            name = "tf-axeal"
-        }
+    workspaces {
+      name = "tf-axeal"
     }
+  }
 }
 
 provider "scaleway" {
@@ -23,164 +23,164 @@ provider "cloudflare" {
 }
 
 resource "scaleway_k8s_cluster_beta" "k8s-cluster" {
-  name = var.scaleway_cluster_name
-  version = var.scaleway_k8s_version
-  cni = var.scaleway_cni
-  ingress = var.scaleway_ingress
+  name              = var.scaleway_cluster_name
+  version           = var.scaleway_k8s_version
+  cni               = var.scaleway_cni
+  ingress           = var.scaleway_ingress
   admission_plugins = var.scaleway_admission_plugins
 }
 
 resource "scaleway_k8s_pool_beta" "k8s-pool-0" {
   cluster_id = scaleway_k8s_cluster_beta.k8s-cluster.id
-  name = var.scaleway_pool_name
-  node_type = var.scaleway_node_type
-  size = var.scaleway_pool_size
+  name       = var.scaleway_pool_name
+  node_type  = var.scaleway_node_type
+  size       = var.scaleway_pool_size
 }
 
 provider "kubernetes" {
   load_config_file = "false"
 
   host  = scaleway_k8s_cluster_beta.k8s-cluster.kubeconfig[0].host
-  token  = scaleway_k8s_cluster_beta.k8s-cluster.kubeconfig[0].token
+  token = scaleway_k8s_cluster_beta.k8s-cluster.kubeconfig[0].token
   cluster_ca_certificate = base64decode(
     scaleway_k8s_cluster_beta.k8s-cluster.kubeconfig[0].cluster_ca_certificate
   )
 }
 
 resource "kubernetes_namespace" "blog" {
-    metadata {
-        name = "blog"
-    }
+  metadata {
+    name = "blog"
+  }
 }
 
 resource "kubernetes_service_account" "blog-deployment-sa" {
-    metadata {
-        name      = "blog-deployment-sa"
-        namespace = "blog"
-    }
+  metadata {
+    name      = "blog-deployment-sa"
+    namespace = "blog"
+  }
 }
 
 resource "kubernetes_role" "blog-deployment-role" {
-    metadata {
-        name      = "blog-deployment-role"
-        namespace = "blog"
-    }
+  metadata {
+    name      = "blog-deployment-role"
+    namespace = "blog"
+  }
 
-    rule {
-        api_groups      = ["apps"]
-        resources       = ["deployments"]
-        verbs           = ["get", "create", "update", "delete", "patch", "watch", "list"]
-    }
+  rule {
+    api_groups = ["apps"]
+    resources  = ["deployments"]
+    verbs      = ["get", "create", "update", "delete", "patch", "watch", "list"]
+  }
 }
 
 resource "kubernetes_role_binding" "blog-deployment-rolebinding" {
-    metadata {
-        name      = "blog-deployment-rolebinding"
-        namespace = "blog"
-    }
-    role_ref {
-        api_group = "rbac.authorization.k8s.io"
-        kind      = "Role"
-        name      = "blog-deployment-role"
-    }
-    subject {
-        kind      = "ServiceAccount"
-        name      = "blog-deployment-sa"
-        namespace = "blog"
-    }
+  metadata {
+    name      = "blog-deployment-rolebinding"
+    namespace = "blog"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = "blog-deployment-role"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = "blog-deployment-sa"
+    namespace = "blog"
+  }
 }
 
 resource "kubernetes_deployment" "blog" {
-    metadata {
-        namespace = "blog"
-        name      = "blog"
+  metadata {
+    namespace = "blog"
+    name      = "blog"
+    labels = {
+      app = "blog"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "blog"
+      }
+    }
+
+    template {
+      metadata {
         labels = {
-            app = "blog"
+          app = "blog"
         }
+      }
+
+      spec {
+        container {
+          image = "axeal/axe.al:latest"
+          name  = "axeal"
+          port {
+            container_port = 8080
+            protocol       = "TCP"
+          }
+        }
+      }
     }
-
-    spec {
-        replicas = 1
-
-        selector {
-            match_labels = {
-                app = "blog"
-            }
-        }
-
-        template {
-            metadata {
-                labels = {
-                    app = "blog"
-                }
-            }
-
-            spec {
-                container {
-                    image = "axeal/axe.al:latest"
-                    name = "axeal"
-                    port {
-                        container_port = 8080
-                        protocol = "TCP"
-                    }
-                }
-            }
-        }
-    }
+  }
 }
 
 resource "kubernetes_secret" "blog-cloudflare-origin" {
-    metadata {
-        name      = "axeal-tls"
-        namespace = "blog"
-    }
+  metadata {
+    name      = "axeal-tls"
+    namespace = "blog"
+  }
 
-    data = {
-        "tls.crt" = var.cloudflare_origin_cert
-        "tls.key"  = var.cloudflare_origin_key
-    }
+  data = {
+    "tls.crt" = var.cloudflare_origin_cert
+    "tls.key" = var.cloudflare_origin_key
+  }
 
-    type = "kubernetes.io/tls"
+  type = "kubernetes.io/tls"
 }
 
 resource "kubernetes_service" "blog" {
-    metadata {
-        name      = "blog"
-        namespace = "blog"
+  metadata {
+    name      = "blog"
+    namespace = "blog"
+  }
+  spec {
+    selector = {
+      app : "blog"
     }
-    spec {
-        selector = {
-            app: "blog"
-        }
-        port {
-            port        = 80
-            target_port = 8080
-            protocol    = "TCP"
-        }
+    port {
+      port        = 80
+      target_port = 8080
+      protocol    = "TCP"
     }
+  }
 }
 
 resource "kubernetes_ingress" "blog" {
-    metadata {
-        namespace = "blog"
-        name      = "blog"
-    }
+  metadata {
+    namespace = "blog"
+    name      = "blog"
+  }
 
-    spec {
-        tls {
-            hosts = ["axe.al"]
-            secret_name = "axeal-tls"
-        }
-        rule {
-            host = "axe.al"
-            http {
-                path {
-                    backend {
-                        service_name = "blog"
-                        service_port = 80
-                    }
-                }
-            }
-        }
+  spec {
+    tls {
+      hosts       = ["axe.al"]
+      secret_name = "axeal-tls"
     }
+    rule {
+      host = "axe.al"
+      http {
+        path {
+          backend {
+            service_name = "blog"
+            service_port = 80
+          }
+        }
+      }
+    }
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,83 +1,83 @@
 variable "scaleway_access_key" {
-    type = string
+  type = string
 }
 
 variable "scaleway_secret_key" {
-    type = string
+  type = string
 }
 
 variable "scaleway_organization_id" {
-    type = string
+  type = string
 }
 
 variable "scaleway_zone" {
-    type = string
-    default = "fr-par-1"
+  type    = string
+  default = "fr-par-1"
 }
 
 variable "scaleway_region" {
-    type = string
-    default = "fr-par"
+  type    = string
+  default = "fr-par"
 }
 
 variable "cloudflare_email" {
-    type = string
+  type = string
 }
 
 variable "cloudflare_api_key" {
-    type = string
+  type = string
 }
 
 variable "scaleway_cluster_name" {
-    type = string
+  type = string
 }
 
 variable "scaleway_k8s_version" {
-    type = string
-    default = "1.18.3"
+  type    = string
+  default = "1.18.3"
 }
 
 variable "scaleway_cni" {
-    type = string
-    default = "cilium"
+  type    = string
+  default = "cilium"
 }
 
 variable "scaleway_ingress" {
-    type = string
-    default = "nginx"
+  type    = string
+  default = "nginx"
 }
 
 variable "scaleway_admission_plugins" {
-    type = list(string)
-    default = []
+  type    = list(string)
+  default = []
 }
 
 variable "scaleway_pool_name" {
-    type = string
+  type = string
 }
 
 variable "scaleway_node_type" {
-    type = string
-    default = "DEV1-M"
+  type    = string
+  default = "DEV1-M"
 }
 
 variable "scaleway_pool_size" {
-    type = number
-    default = 1
+  type    = number
+  default = 1
 }
 
 variable "cloudflare_zone_id" {
-    type = string
+  type = string
 }
 
 variable "cloudflare_record_name" {
-    type = string
+  type = string
 }
 
 variable "cloudflare_origin_cert" {
-    type = string
+  type = string
 }
 
 variable "cloudflare_origin_key" {
-    type = string
+  type = string
 }
