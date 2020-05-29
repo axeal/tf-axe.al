e7198d7db6d9476cd89fd0d02d5c541987101f1a
Alex Seymour <axeal@users.noreply.github.com>
2020-05-29 20:00:35 +0200

    Use kustomize

--- main.tf
+++ main.tf
@@ -37,150 +37,27 @@ resource "scaleway_k8s_pool_beta" "k8s-pool-0" {
   size       = var.scaleway_pool_size
 }
 
-provider "kubernetes" {
-  load_config_file = "false"
-
-  host  = scaleway_k8s_cluster_beta.k8s-cluster.kubeconfig[0].host
-  token = scaleway_k8s_cluster_beta.k8s-cluster.kubeconfig[0].token
-  cluster_ca_certificate = base64decode(
-    scaleway_k8s_cluster_beta.k8s-cluster.kubeconfig[0].cluster_ca_certificate
-  )
+resource "local_file" "tls-crt" {
+  content  = var.cloudflare_origin_cert
+  filename = "${path.module}/manifests/blog/base/secrets/tls.crt"
 }
 
-resource "kubernetes_namespace" "blog" {
-  metadata {
-    name = "blog"
-  }
+resource "local_file" "tls-key" {
+  content           = var.cloudflare_origin_key
+  filename          = "${path.module}/manifests/blog/base/secrets/tls.key"
+  sensitive_content = true
 }
 
-resource "kubernetes_service_account" "blog-deployment-sa" {
-  metadata {
-    name      = "blog-deployment-sa"
-    namespace = "blog"
-  }
+provider "kustomization" {
+  kubeconfig_raw = scaleway_k8s_cluster_beta.k8s-cluster.kubeconfig[0].config_file
 }
 
-resource "kubernetes_role" "blog-deployment-role" {
-  metadata {
-    name      = "blog-deployment-role"
-    namespace = "blog"
-  }
-
-  rule {
-    api_groups = ["apps"]
-    resources  = ["deployments"]
-    verbs      = ["get", "create", "update", "delete", "patch", "watch", "list"]
-  }
+data "kustomization" "blog" {
+  path = "manifests/blog/base"
 }
 
-resource "kubernetes_role_binding" "blog-deployment-rolebinding" {
-  metadata {
-    name      = "blog-deployment-rolebinding"
-    namespace = "blog"
-  }
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "Role"
-    name      = "blog-deployment-role"
-  }
-  subject {
-    kind      = "ServiceAccount"
-    name      = "blog-deployment-sa"
-    namespace = "blog"
-  }
-}
+resource "kustomization_resource" "blog" {
+  for_each = data.kustomization.blog.ids
 
-resource "kubernetes_deployment" "blog" {
-  metadata {
-    namespace = "blog"
-    name      = "blog"
-    labels = {
-      app = "blog"
-    }
-  }
-
-  spec {
-    replicas = 1
-
-    selector {
-      match_labels = {
-        app = "blog"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "blog"
-        }
-      }
-
-      spec {
-        container {
-          image = "axeal/axe.al:latest"
-          name  = "axeal"
-          port {
-            container_port = 8080
-            protocol       = "TCP"
-          }
-        }
-      }
-    }
-  }
-}
-
-resource "kubernetes_secret" "blog-cloudflare-origin" {
-  metadata {
-    name      = "axeal-tls"
-    namespace = "blog"
-  }
-
-  data = {
-    "tls.crt" = var.cloudflare_origin_cert
-    "tls.key" = var.cloudflare_origin_key
-  }
-
-  type = "kubernetes.io/tls"
-}
-
-resource "kubernetes_service" "blog" {
-  metadata {
-    name      = "blog"
-    namespace = "blog"
-  }
-  spec {
-    selector = {
-      app : "blog"
-    }
-    port {
-      port        = 80
-      target_port = 8080
-      protocol    = "TCP"
-    }
-  }
-}
-
-resource "kubernetes_ingress" "blog" {
-  metadata {
-    namespace = "blog"
-    name      = "blog"
-  }
-
-  spec {
-    tls {
-      hosts       = ["axe.al"]
-      secret_name = "axeal-tls"
-    }
-    rule {
-      host = "axe.al"
-      http {
-        path {
-          backend {
-            service_name = "blog"
-            service_port = 80
-          }
-        }
-      }
-    }
-  }
+  manifest = data.kustomization.blog.manifests[each.value]
 }
