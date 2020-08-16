2b573ef68cc6e27902e381bdb72db70135b23f0d
Alex Seymour <axeal@users.noreply.github.com>
2020-08-16 20:24:35 +0100

    Add required_providers

--- main.tf
+++ main.tf
@@ -17,7 +17,6 @@ provider "scaleway" {
 }
 
 provider "cloudflare" {
-  version   = "~> 2.0"
   api_token = var.cloudflare_api_token
 }
 
--- versions.tf
+++ versions.tf
@@ -1,3 +1,21 @@
 terraform {
   required_version = ">= 0.12"
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "2.9.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "1.2.4"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "1.12.0"
+    }
+    scaleway = {
+      source  = "scaleway/scaleway"
+      version = "1.16.0"
+    }
+  }
 }
